7d85a0c276f2fb110b414043ea15c6c2e9c6f89b
Devashish2910 <dgpatel2910@gmail.com>
2018-02-25 04:22:26 -0600

    added: test case- Nested Populate

---
+++ test/read_test.js
@@ -0,0 +1,45 @@
+const User = require('./../collections/users');
+const Blog = require('./../collections/blogs');
+const Comment = require('./../collections/comments');
+const assert = require('assert');
+
+describe('CREATE', () => {
+  let deva, blog, comment;
+  beforeEach(done => {
+    deva = new User({fName: 'Devashish', lName: 'Patel', email: 'dgpatel2910@gmail.com'});
+    blog = new Blog({title: "My first blog", content: 'Welcome to my blog', likes: 0});
+    comment = new Comment({content: 'My first comment'});
+
+    deva.blogs.push(blog);
+    blog.author = deva;
+    blog.comments.push(comment);
+    comment.blog = blog;
+    comment.author = deva;
+
+    Promise.all([deva.save(), blog.save(), comment.save()])
+     .then(() => {
+       done();
+     });
+  });
+
+  it('Nested Populate', done => {
+    User.findOne({fName: 'Devashish'})
+     .populate({
+       path: 'blogs',
+       populate: {
+         path: 'author',
+         model: 'users',
+         path: 'comments',
+         model: 'comments',
+         populate: {
+           path: 'author',
+           model: 'users'
+         }
+       }
+     })
+     .then(res => {
+       assert(res.blogs[0].comments[0].author.fName === 'Devashish')
+       done();
+     })
+  });
+});
