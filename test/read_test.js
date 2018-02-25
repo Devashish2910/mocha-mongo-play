fffb4364f7f57248dd5352489bbfa8e0c3fc3d35
Devashish2910 <dgpatel2910@gmail.com>
2018-02-25 07:00:54 -0600

    added: test case-pagination

--- test/read_test.js
+++ test/read_test.js
@@ -4,9 +4,14 @@ const Comment = require('./../collections/comments');
 const assert = require('assert');
 
 describe('CREATE', () => {
-  let deva, blog, comment;
+  let deva, blog, comment, d, e, v, a;
   beforeEach(done => {
     deva = new User({fName: 'Devashish', lName: 'Patel', email: 'dgpatel2910@gmail.com'});
+    d = new User({fName: 'D', lName: 'Patel', email: 'd@gmail.com'});
+    e = new User({fName: 'E', lName: 'Patel', email: 'e@gmail.com'});
+    v = new User({fName: 'V', lName: 'Patel', email: 'v@gmail.com'});
+    a = new User({fName: 'A', lName: 'Patel', email: 'a@gmail.com'});
+
     blog = new Blog({title: "My first blog", content: 'Welcome to my blog', likes: 0});
     comment = new Comment({content: 'My first comment'});
 
@@ -16,7 +21,7 @@ describe('CREATE', () => {
     comment.blog = blog;
     comment.author = deva;
 
-    Promise.all([deva.save(), blog.save(), comment.save()])
+    Promise.all([deva.save(), blog.save(), comment.save(), d.save(), e.save(), v.save(), a.save()])
      .then(() => {
        done();
      });
@@ -39,6 +44,17 @@ describe('CREATE', () => {
      })
      .then(res => {
        assert(res.blogs[0].comments[0].author.fName === 'Devashish')
+       done();
+     })
+  });
+
+  it.only('Pagination', done => {
+    User.find({})
+     .sort({fName: -1})
+     .skip(0)
+     .limit(4)
+     .then(res => {
+       console.log(res);
        done();
      })
   });
